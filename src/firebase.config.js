7894b5c0d2101660a320754f8bd30abac34012af
Sayed Mahmud <90099423+Mahmud-ii@users.noreply.github.com>
2023-12-19 19:14:07 +0600

    Update firebase.config.js added env variables

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -7,7 +7,14 @@ import { getStorage } from "firebase/storage";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {};
+const firebaseConfig = {
+  apiKey:  process.env.API_KEY,
+  authDomain:   process.env.AUTH_DOMAIN,
+  projectId:  process.env.PROJECT_ID,
+  storageBucket:   process.env.STORAGE_BUCKET,
+  messagingSenderId:  process.env.MESSAGING_SENDER_ID,
+  appId:   process.env.APP_ID
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
